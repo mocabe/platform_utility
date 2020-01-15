e24c0367b09869e07c002d985f83962e30fbf036
mocabe <hisa.mocabe@gmail.com>
2020-01-15 18:49:38 +0900

    intrin to lower-case

--- include/platform/intrin.hpp
+++ include/platform/intrin.hpp
@@ -19,129 +19,129 @@ namespace PLATFORM_UTILITY_NAMESPACE {
 
 #if defined(__MMX__)
 #  define PLATFORM_INTRIN_MMX 1
-  constexpr bool has_MMX = true;
+  constexpr bool has_mmx = true;
 #else
-  constexpr bool has_MMX    = false;
+  constexpr bool has_mmx    = false;
 #endif
 
 #if defined(__AES__)
 #  define PLATFORM_INTRIN_AES 1
-  constexpr bool has_AES = true;
+  constexpr bool has_aes = true;
 #else
-  constexpr bool has_AES    = false;
+  constexpr bool has_aes    = false;
 #endif
 
 #if defined(__FMA__)
 #  define PLATFORM_INTRIN_FMA 1
-  constexpr bool has_FMA = true;
+  constexpr bool has_fma = true;
 #else
-  constexpr bool has_FMA    = false;
+  constexpr bool has_fma    = false;
 #endif
 
 #if defined(__SSE__)
 #  define PLATFORM_INTRIN_SSE 1
-  constexpr bool has_SSE = true;
+  constexpr bool has_sse = true;
 #else
-  constexpr bool has_SSE    = false;
+  constexpr bool has_sse    = false;
 #endif
 
 #if defined(__SSE2__)
 #  define PLATFORM_INTRIN_SSE2 1
-  constexpr bool has_SSE2 = true;
+  constexpr bool has_sse2 = true;
 #else
-  constexpr bool has_SSE2   = false;
+  constexpr bool has_sse2   = false;
 #endif
 
 #if defined(__SSE3__)
 #  define PLATFORM_INTRIN_SSE3 1
-  constexpr bool has_SSE3 = true;
+  constexpr bool has_sse3 = true;
 #else
-  constexpr bool has_SSE3   = false;
+  constexpr bool has_sse3   = false;
 #endif
 
 #if defined(__SSSE3__)
 #  define PLATFORM_INTRIN_SSE3 1
-  constexpr bool has_SSSE3 = true;
+  constexpr bool has_ssse3 = true;
 #else
-  constexpr bool has_SSSE3  = false;
+  constexpr bool has_ssse3  = false;
 #endif
 
 #if defined(__SSE4_1__)
 #  define PLATFORM_INTRIN_SSE4_1 1
-  constexpr bool has_SSE4_1 = true;
+  constexpr bool has_sse4_1 = true;
 #else
-  constexpr bool has_SSE4_1 = false;
+  constexpr bool has_sse4_1 = false;
 #endif
 
 #if defined(__SSE4_2__)
 #  define PLATFORM_INTRIN_SSE4_2 1
-  constexpr bool has_SSE4_2 = true;
+  constexpr bool has_sse4_2 = true;
 #else
-  constexpr bool has_SSE4_2 = false;
+  constexpr bool has_sse4_2 = false;
 #endif
 
 #if defined(__AVX__)
 #  define PLATFORM_INTRIN_AVX 1
-  constexpr bool has_AVX = true;
+  constexpr bool has_avx = true;
 #else
-  constexpr bool has_AVX    = false;
+  constexpr bool has_avx    = false;
 #endif
 
 #if defined(__AVX2__)
 #  define PLATFORM_INTRIN_AVX2 1
-  constexpr bool has_AVX2 = true;
+  constexpr bool has_avx2 = true;
 #else
-  constexpr bool has_AVX2     = false;
+  constexpr bool has_avx2     = false;
 #endif
 
 #if defined(__AVX512F__) // Foundation
 #  define PLATFORM_INTRIN_AVX512F 1
-  constexpr bool has_AVX512F = true;
+  constexpr bool has_avx512f = true;
 #else
-  constexpr bool has_AVX512F  = false;
+  constexpr bool has_avx512f  = false;
 #endif
 
 #if defined(__AVX512DQ__) // Doubleword and Quadword
 #  define PLATFORM_INTRIN_AVX512DQ 1
-  constexpr bool has_AVX512DQ = true;
+  constexpr bool has_avx512dq = true;
 #else
-  constexpr bool has_AVX512DQ = false;
+  constexpr bool has_avx512dq = false;
 #endif
 
 #if defined(__AVX512BW__) // Byte and Word
 #  define PLATFORM_INTRIN_AVX512BW 1
-  constexpr bool has_AVX512BW = true;
+  constexpr bool has_avx512bw = true;
 #else
-  constexpr bool has_AVX512BW = false;
+  constexpr bool has_avx512bw = false;
 #endif
 
 #if defined(__AVX512CD__) // Conflict Detection
 #  define PLATFORM_INTRIN_AVX512CD 1
-  constexpr bool has_AVX512CD = true;
+  constexpr bool has_avx512cd = true;
 #else
-  constexpr bool has_AVX512CD = false;
+  constexpr bool has_avx512cd = false;
 #endif
 
 #if defined(__AVX512ER__) // Exponential and Reciprocal
 #  define PLATFORM_INTRIN_AVX512ER 1
-  constexpr bool has_AVX512ER = true;
+  constexpr bool has_avx512er = true;
 #else
-  constexpr bool has_AVX512ER = false;
+  constexpr bool has_avx512er = false;
 #endif
 
 
 #if defined(__AVX512PF__) // Prefetch Instructions
 #  define PLATFORM_INTRIN_AVX512PF 1
-  constexpr bool has_AVX512PF = true;
+  constexpr bool has_avx512pf = true;
 #else
-  constexpr bool has_AVX512PF = false;
+  constexpr bool has_avx512pf = false;
 #endif
 
 #if defined(__AVX512VL__) // Vector Length
 #  define PLATFORM_INTRIN_AVX512VL 1
-  constexpr bool has_AVX512VL = true;
+  constexpr bool has_avx512vl = true;
 #else
-  constexpr bool has_AVX512VL = false;
+  constexpr bool has_avx512vl = false;
 #endif
 
 }
